84af7336f35daba1bbd707b6096154ff57d625dd
Rosana EL JURDI <44140190+rosanajurdi@users.noreply.github.com>
2022-03-10 23:47:46 +0100

    Update KFOLD_Split_Dataset.py

--- KFOLD_Split_Dataset.py
+++ KFOLD_Split_Dataset.py
@@ -10,25 +10,22 @@ Script to divide the data into folds,
 2) root_path denotes the absolute path to the dataset + ROOT 
 3) fold: the fold you need to create.
 4)
+# variables to initialize
+typ = 'ROOT': the root folder you want your downloaded dataset to be in. preferably place it in ROOT. 
+root_path: the root directory leading to your data.
+fold: the name of the target fold 
+nb_val: the number of validation samples (recommended to be 20% of the total training set)
 '''
-from DataSEt_Classes import Decathlon
-from Label_Estimate_Helper_Functions import extract_bbox, rect_mask, get__mask, Get_Upper_Lower_boundaries
-from torchvision import transforms
-from matplotlib import cm
-import matplotlib.pyplot as plt
-import numpy as np
-import imageio
-import h5py
-import sys
 import os
-import shutil
 import random
-sys.path.append('../medicaltorch-0.2')
-sys.path.append('/home/eljurros/spare-workplace/Multi_Organ_Segmentation/DataSet_Functions')
+import shutil
+from DataSEt_Classes import Decathlon
 
 typ = 'ROOT'
-root_path = '/media/eljurros/Transcend/Decathlone/Task02_Heart/nifty'
+root_path = '/Users/rosana.eljurdi/Desktop/Benchmark/Task02_Heart/nifty'
 fold = 'FOLD_DUmmy'
+nb_val = 400
+
 if os.path.exists(os.path.join(root_path, fold)) is False:
     os.mkdir(os.path.join(root_path,  fold))
     os.mkdir(os.path.join(root_path,   fold, 'train'))
@@ -42,7 +39,7 @@ if os.path.exists(train_path[0]) is False:
     os.mkdir(train_path[0])
     os.mkdir(train_path[1])
 
-nb_val = 400
+
 if os.path.exists(val_path[0]) is False:
     os.mkdir(val_path[0])
     os.mkdir(val_path[1])
@@ -52,8 +49,6 @@ for i, patient_path in enumerate(ds.filename_pairs):
     patient_name = os.path.basename(patient_path[0])
     input_filename, gt_filename = patient_path[0], \
                                   patient_path[1]
-    
-    
 
     i = random.randint(1,101)
         
